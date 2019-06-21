9e69259e38dbee596249e389a5b791e59471d792
alisoltani <ali.soltani@icloud.com>
2019-06-20 22:47:05 -0400

    Update supervised_learning_owl_challenge2.py

--- spider/pipelines/supervised_learning_owl_challenge2.py
+++ spider/pipelines/supervised_learning_owl_challenge2.py
@@ -119,7 +119,7 @@ class OWLRegressionPipelineChallenge2(BasePipeline):
         step_6.add_hyperparameter(
                 name='weight_min_off',
                 argument_type=ArgumentType.VALUE,
-                data=13)
+                data=9)
         step_6.add_argument(
                 name='inputs',
                 argument_type=ArgumentType.CONTAINER,
