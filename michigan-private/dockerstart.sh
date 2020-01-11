bd1d461348cbc9a121bd4f13a15b3659e3e2d926
Ryan Szeto <szetor@umich.edu>
2020-01-11 20:40:35 +0000

    Rename primitive preparation script; set Git user and primitive export owner

--- michigan-private/dockerstart.sh
+++ michigan-private/dockerstart.sh
@@ -12,6 +12,12 @@ docker run -d \
   --mount type=bind,source="$(pwd)",target=/spider \
   --mount type=bind,source=/z/mid/D3M/datasets_public,target=/datasets \
   --mount type=bind,source=/z/mid/D3M/volumes,target=/volumes \
+  --env "HOST_USER=$(id -u)" \
+  --env "HOST_GROUP=$(id -g)" \
+  --env "GIT_COMMITTER_NAME=$(git config user.name)" \
+  --env "GIT_COMMITTER_EMAIL=$(git config user.email)" \
+  --env "GIT_AUTHOR_NAME=$(git config user.name)" \
+  --env "GIT_AUTHOR_EMAIL=$(git config user.email)" \
   spider
 
 docker exec -it spider_test bash
--- michigan-private/prepare_primitive_export.sh
+++ michigan-private/prepare_primitive_export.sh
@@ -3,10 +3,15 @@
 SCRIPT_DIR="$(cd $(dirname $0); pwd)"
 PROJ_DIR="$SCRIPT_DIR/.."
 
+# Compress pipeline run logs
 cd "$PROJ_DIR"
 for f in `find Michigan -name *yaml`; do
     echo "Compressing $f..."
     gzip $f
 done
+
+# Change ownership of `Michigan` to the host user
+chown -R $HOST_USER:$HOST_GROUP Michigan
+
 echo "Done."
 
