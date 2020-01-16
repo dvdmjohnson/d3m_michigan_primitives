ef7ac97d9a77767a8a4e36c7757b36ae49e13956
Ryan Szeto <szetor@umich.edu>
2020-01-16 11:36:36 -0500

    Simplify pipeline info dict

--- michigan-private/genjson.py
+++ michigan-private/genjson.py
@@ -29,72 +29,26 @@ version = pkg_resources.get_distribution('spider').version
 # E.g. d3m.primitives.learner.goturn.Umich
 primitive_module_names = ['d3m.primitives.{}'.format(x) for x in pkg_resources.get_entry_map('spider')['d3m.primitives'].keys()]
 
+fit_score_command = 'python3 -m d3m runtime -v /volumes fit-score -p Michigan/{primitive}/{version}/pipelines/{instanceid}.json -r /datasets/seed_datasets_current/{dataset}/{dataset}_problem/problemDoc.json -i /datasets/seed_datasets_current/{dataset}/TRAIN/dataset_TRAIN/datasetDoc.json -t /datasets/seed_datasets_current/{dataset}/TEST/dataset_TEST/datasetDoc.json -a /datasets/seed_datasets_current/{dataset}/SCORE/dataset_TEST/datasetDoc.json -o /dev/null -O Michigan/{primitive}/0.0.5/pipeline_runs/{pipeline}.yaml > pipeline_results/{pipeline}.txt'
+
 # Stores information required to generate the command to run each pipeline
 pipeline_cmd_template_info = {
-    OWLRegressionPipeline: {
-        'primitive': 'd3m.primitives.regression.owl_regression.Umich',
-        'command': "python3 -m d3m runtime -v /volumes fit-score -p Michigan/{primitive}/{version}/pipelines/{instanceid}.json -r /datasets/seed_datasets_current/{dataset}/{dataset}_problem/problemDoc.json -i /datasets/seed_datasets_current/{dataset}/TRAIN/dataset_TRAIN/datasetDoc.json -t /datasets/seed_datasets_current/{dataset}/TEST/dataset_TEST/datasetDoc.json -a /datasets/seed_datasets_current/{dataset}/SCORE/dataset_TEST/datasetDoc.json -o /dev/null -O Michigan/{primitive}/0.0.5/pipeline_runs/{pipeline}.yaml > pipeline_results/{pipeline}.txt"
-    },
-    # OWLRegressionPipelineChallenge1: {
-    #     'primitive': 'd3m.primitives.regression.owl_regression.Umich',
-    #     'command': "python3 -m d3m runtime -v /volumes fit-score -p Michigan/{primitive}/{version}/pipelines/{instanceid}.json -r /datasets/seed_datasets_current/{dataset}/{dataset}_problem/problemDoc.json -i /datasets/seed_datasets_current/{dataset}/TRAIN/dataset_TRAIN/datasetDoc.json -t /datasets/seed_datasets_current/{dataset}/TEST/dataset_TEST/datasetDoc.json -a /datasets/seed_datasets_current/{dataset}/SCORE/dataset_TEST/datasetDoc.json -o /dev/null -O Michigan/{primitive}/0.0.5/pipeline_runs/{pipeline}.yaml > pipeline_results/{pipeline}.txt"
-    # },
-    OWLRegressionPipelineChallenge2: {
-        'primitive': 'd3m.primitives.regression.owl_regression.Umich',
-        'command': "python3 -m d3m runtime -v /volumes fit-score -p Michigan/{primitive}/{version}/pipelines/{instanceid}.json -r /datasets/seed_datasets_current/{dataset}/{dataset}_problem/problemDoc.json -i /datasets/seed_datasets_current/{dataset}/TRAIN/dataset_TRAIN/datasetDoc.json -t /datasets/seed_datasets_current/{dataset}/TEST/dataset_TEST/datasetDoc.json -a /datasets/seed_datasets_current/{dataset}/SCORE/dataset_TEST/datasetDoc.json -o /dev/null -O Michigan/{primitive}/0.0.5/pipeline_runs/{pipeline}.yaml > pipeline_results/{pipeline}.txt"
-    },
-    OWLRegressionPipelineChallenge3: {
-        'primitive': 'd3m.primitives.regression.owl_regression.Umich',
-        'command': "python3 -m d3m runtime -v /volumes fit-score -p Michigan/{primitive}/{version}/pipelines/{instanceid}.json -r /datasets/seed_datasets_current/{dataset}/{dataset}_problem/problemDoc.json -i /datasets/seed_datasets_current/{dataset}/TRAIN/dataset_TRAIN/datasetDoc.json -t /datasets/seed_datasets_current/{dataset}/TEST/dataset_TEST/datasetDoc.json -a /datasets/seed_datasets_current/{dataset}/SCORE/dataset_TEST/datasetDoc.json -o /dev/null -O Michigan/{primitive}/0.0.5/pipeline_runs/{pipeline}.yaml > pipeline_results/{pipeline}.txt"
-    },
-    # FeaturizationVGG16Pipeline: {
-    #     'primitive': 'd3m.primitives.feature_extraction.vgg16.Umich',
-    #     'command': "python3 -m d3m runtime -v /volumes fit-score -p Michigan/{primitive}/{version}/pipelines/{instanceid}.json -r /datasets/seed_datasets_current/{dataset}/{dataset}_problem/problemDoc.json -i /datasets/seed_datasets_current/{dataset}/TRAIN/dataset_TRAIN/datasetDoc.json -t /datasets/seed_datasets_current/{dataset}/TEST/dataset_TEST/datasetDoc.json -a /datasets/seed_datasets_current/{dataset}/SCORE/dataset_TEST/datasetDoc.json -o /dev/null -O Michigan/{primitive}/0.0.5/pipeline_runs/{pipeline}.yaml > pipeline_results/{pipeline}.txt"
-    # },
-    # GO_DECPipeline: {
-    #     'primitive': 'd3m.primitives.data_compression.go_dec.Umich',
-    #     'command': "python3 -m d3m runtime -v /volumes fit-score -p Michigan/{primitive}/{version}/pipelines/{instanceid}.json -r /datasets/seed_datasets_current/{dataset}/{dataset}_problem/problemDoc.json -i /datasets/seed_datasets_current/{dataset}/TRAIN/dataset_TRAIN/datasetDoc.json -t /datasets/seed_datasets_current/{dataset}/TEST/dataset_TEST/datasetDoc.json -a /datasets/seed_datasets_current/{dataset}/SCORE/dataset_TEST/datasetDoc.json -o /dev/null -O Michigan/{primitive}/0.0.5/pipeline_runs/{pipeline}.yaml > pipeline_results/{pipeline}.txt"
-    # },
-    # PCP_IALMPipeline: {
-    #     'primitive': 'd3m.primitives.data_compression.pcp_ialm.Umich',
-    #     'command': "python3 -m d3m runtime -v /volumes fit-score -p Michigan/{primitive}/{version}/pipelines/{instanceid}.json -r /datasets/seed_datasets_current/{dataset}/{dataset}_problem/problemDoc.json -i /datasets/seed_datasets_current/{dataset}/TRAIN/dataset_TRAIN/datasetDoc.json -t /datasets/seed_datasets_current/{dataset}/TEST/dataset_TEST/datasetDoc.json -a /datasets/seed_datasets_current/{dataset}/SCORE/dataset_TEST/datasetDoc.json -o /dev/null -O Michigan/{primitive}/0.0.5/pipeline_runs/{pipeline}.yaml > pipeline_results/{pipeline}.txt"
-    # },
-    # RPCA_LBDPipeline: {
-    #     'primitive': 'd3m.primitives.data_compression.rpca_lbd.Umich',
-    #     'command': "python3 -m d3m runtime -v /volumes fit-score -p Michigan/{primitive}/{version}/pipelines/{instanceid}.json -r /datasets/seed_datasets_current/{dataset}/{dataset}_problem/problemDoc.json -i /datasets/seed_datasets_current/{dataset}/TRAIN/dataset_TRAIN/datasetDoc.json -t /datasets/seed_datasets_current/{dataset}/TEST/dataset_TEST/datasetDoc.json -a /datasets/seed_datasets_current/{dataset}/SCORE/dataset_TEST/datasetDoc.json -o /dev/null -O Michigan/{primitive}/0.0.5/pipeline_runs/{pipeline}.yaml > pipeline_results/{pipeline}.txt"
-    # },
-    GRASTAPipeline: {
-        'primitive': 'd3m.primitives.data_compression.grasta.Umich',
-        'command': "python3 -m d3m runtime -v /volumes fit-score -p Michigan/{primitive}/{version}/pipelines/{instanceid}.json -r /datasets/seed_datasets_current/{dataset}/{dataset}_problem/problemDoc.json -i /datasets/seed_datasets_current/{dataset}/TRAIN/dataset_TRAIN/datasetDoc.json -t /datasets/seed_datasets_current/{dataset}/TEST/dataset_TEST/datasetDoc.json -a /datasets/seed_datasets_current/{dataset}/SCORE/dataset_TEST/datasetDoc.json -o /dev/null -O Michigan/{primitive}/0.0.5/pipeline_runs/{pipeline}.yaml > pipeline_results/{pipeline}.txt"
-    },
-    GRASTAPipelineChallenge1: {
-        'primitive': 'd3m.primitives.data_compression.grasta.Umich',
-        'command': "python3 -m d3m runtime -v /volumes fit-score -p Michigan/{primitive}/{version}/pipelines/{instanceid}.json -r /datasets/seed_datasets_current/{dataset}/{dataset}_problem/problemDoc.json -i /datasets/seed_datasets_current/{dataset}/TRAIN/dataset_TRAIN/datasetDoc.json -t /datasets/seed_datasets_current/{dataset}/TEST/dataset_TEST/datasetDoc.json -a /datasets/seed_datasets_current/{dataset}/SCORE/dataset_TEST/datasetDoc.json -o /dev/null -O Michigan/{primitive}/0.0.5/pipeline_runs/{pipeline}.yaml > pipeline_results/{pipeline}.txt"
-    },
-    # GRASTAPipelineChallenge2: {
-    #     'primitive': 'd3m.primitives.data_compression.grasta.Umich',
-    #     'command': "python3 -m d3m runtime -v /volumes fit-score -p Michigan/{primitive}/{version}/pipelines/{instanceid}.json -r /datasets/seed_datasets_current/{dataset}/{dataset}_problem/problemDoc.json -i /datasets/seed_datasets_current/{dataset}/TRAIN/dataset_TRAIN/datasetDoc.json -t /datasets/seed_datasets_current/{dataset}/TEST/dataset_TEST/datasetDoc.json -a /datasets/seed_datasets_current/{dataset}/SCORE/dataset_TEST/datasetDoc.json -o /dev/null -O Michigan/{primitive}/0.0.5/pipeline_runs/{pipeline}.yaml > pipeline_results/{pipeline}.txt"
-    # },
-    KSSPipeline: {
-        'primitive': 'd3m.primitives.clustering.kss.Umich',
-        'command': "python3 -m d3m runtime -v /volumes fit-score -p Michigan/{primitive}/{version}/pipelines/{instanceid}.json -r /datasets/seed_datasets_current/{dataset}/{dataset}_problem/problemDoc.json -i /datasets/seed_datasets_current/{dataset}/TRAIN/dataset_TRAIN/datasetDoc.json -t /datasets/seed_datasets_current/{dataset}/TEST/dataset_TEST/datasetDoc.json -a /datasets/seed_datasets_current/{dataset}/SCORE/dataset_TEST/datasetDoc.json -o /dev/null -O Michigan/{primitive}/0.0.5/pipeline_runs/{pipeline}.yaml > pipeline_results/{pipeline}.txt"
-    },
-    EKSSPipeline: {
-        'primitive': 'd3m.primitives.clustering.ekss.Umich',
-        'command': "python3 -m d3m runtime -v /volumes fit-score -p Michigan/{primitive}/{version}/pipelines/{instanceid}.json -r /datasets/seed_datasets_current/{dataset}/{dataset}_problem/problemDoc.json -i /datasets/seed_datasets_current/{dataset}/TRAIN/dataset_TRAIN/datasetDoc.json -t /datasets/seed_datasets_current/{dataset}/TEST/dataset_TEST/datasetDoc.json -a /datasets/seed_datasets_current/{dataset}/SCORE/dataset_TEST/datasetDoc.json -o /dev/null -O Michigan/{primitive}/0.0.5/pipeline_runs/{pipeline}.yaml > pipeline_results/{pipeline}.txt"
-    },
-    SSCADMMPipeline: {
-        'primitive': 'd3m.primitives.clustering.ssc_admm.Umich',
-        'command': "python3 -m d3m runtime -v /volumes fit-score -p Michigan/{primitive}/{version}/pipelines/{instanceid}.json -r /datasets/seed_datasets_current/{dataset}/{dataset}_problem/problemDoc.json -i /datasets/seed_datasets_current/{dataset}/TRAIN/dataset_TRAIN/datasetDoc.json -t /datasets/seed_datasets_current/{dataset}/TEST/dataset_TEST/datasetDoc.json -a /datasets/seed_datasets_current/{dataset}/SCORE/dataset_TEST/datasetDoc.json -o /dev/null -O Michigan/{primitive}/0.0.5/pipeline_runs/{pipeline}.yaml > pipeline_results/{pipeline}.txt"
-    },
-    SSCCVXPipeline: {
-        'primitive': 'd3m.primitives.clustering.ssc_cvx.Umich',
-        'command': "python3 -m d3m runtime -v /volumes fit-score -p Michigan/{primitive}/{version}/pipelines/{instanceid}.json -r /datasets/seed_datasets_current/{dataset}/{dataset}_problem/problemDoc.json -i /datasets/seed_datasets_current/{dataset}/TRAIN/dataset_TRAIN/datasetDoc.json -t /datasets/seed_datasets_current/{dataset}/TEST/dataset_TEST/datasetDoc.json -a /datasets/seed_datasets_current/{dataset}/SCORE/dataset_TEST/datasetDoc.json -o /dev/null -O Michigan/{primitive}/0.0.5/pipeline_runs/{pipeline}.yaml > pipeline_results/{pipeline}.txt"
-    },
-    SSCOMPPipeline: {
-        'primitive': 'd3m.primitives.clustering.ssc_omp.Umich',
-        'command': "python3 -m d3m runtime -v /volumes fit-score -p Michigan/{primitive}/{version}/pipelines/{instanceid}.json -r /datasets/seed_datasets_current/{dataset}/{dataset}_problem/problemDoc.json -i /datasets/seed_datasets_current/{dataset}/TRAIN/dataset_TRAIN/datasetDoc.json -t /datasets/seed_datasets_current/{dataset}/TEST/dataset_TEST/datasetDoc.json -a /datasets/seed_datasets_current/{dataset}/SCORE/dataset_TEST/datasetDoc.json -o /dev/null -O Michigan/{primitive}/0.0.5/pipeline_runs/{pipeline}.yaml > pipeline_results/{pipeline}.txt > pipeline_results/{pipeline}.txt"
-    },
+    OWLRegressionPipeline: 'd3m.primitives.regression.owl_regression.Umich',
+    # OWLRegressionPipelineChallenge1: 'd3m.primitives.regression.owl_regression.Umich',
+    OWLRegressionPipelineChallenge2: 'd3m.primitives.regression.owl_regression.Umich',
+    OWLRegressionPipelineChallenge3: 'd3m.primitives.regression.owl_regression.Umich',
+    # FeaturizationVGG16Pipeline: 'd3m.primitives.feature_extraction.vgg16.Umich',
+    # GO_DECPipeline: 'd3m.primitives.data_compression.go_dec.Umich',
+    # PCP_IALMPipeline: 'd3m.primitives.data_compression.pcp_ialm.Umich',
+    # RPCA_LBDPipeline: 'd3m.primitives.data_compression.rpca_lbd.Umich',
+    GRASTAPipeline: 'd3m.primitives.data_compression.grasta.Umich',
+    GRASTAPipelineChallenge1: 'd3m.primitives.data_compression.grasta.Umich',
+    # GRASTAPipelineChallenge2: 'd3m.primitives.data_compression.grasta.Umich',
+    KSSPipeline: 'd3m.primitives.clustering.kss.Umich',
+    EKSSPipeline: 'd3m.primitives.clustering.ekss.Umich',
+    SSCADMMPipeline: 'd3m.primitives.clustering.ssc_admm.Umich',
+    SSCCVXPipeline: 'd3m.primitives.clustering.ssc_cvx.Umich',
+    SSCOMPPipeline: 'd3m.primitives.clustering.ssc_omp.Umich',
 }
 
 pipeline_cmds = []
@@ -119,12 +73,12 @@ for pl in pipeline_cmd_template_info.keys():
     json_info = instance.get_json()
     instanceid = instance.get_id()
     dataset = instance.dataset
-    prim = pipeline_cmd_template_info[pl]['primitive']
+    prim = pipeline_cmd_template_info[pl]
 
     print(prim, pl_name, '->', instanceid)
 
     # Fill out the template for this pipeline's run command
-    pipeline_cmd = pipeline_cmd_template_info[pl]['command'].format(
+    pipeline_cmd = fit_score_command.format(
         version=version,
         primitive=prim,
         pipeline=pl_name,
