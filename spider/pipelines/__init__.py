021051d65719c68bcfabff9e514b74657f4c0fe0
Ryan Szeto <szetor@umich.edu>
2020-01-16 10:52:25 -0500

    Rename pipeline files

--- spider/pipelines/__init__.py
+++ spider/pipelines/__init__.py
@@ -1,40 +1,19 @@
-from .featurization_audiofeaturization import FeaturizationAudioFeaturizationPipeline
-from .featurization_i3d import FeaturizationI3DPipeline
-from .featurization_vgg16 import FeaturizationVGG16Pipeline
-from .supervised_learning_owl import OWLRegressionPipeline
-from .supervised_learning_owl_challenge1 import OWLRegressionPipelineChallenge1
-from .supervised_learning_owl_challenge2 import OWLRegressionPipelineChallenge2
-from .supervised_learning_owl_challenge3 import OWLRegressionPipelineChallenge3
-from .cluster_ssc_cvxcluster import SSCCVXPipeline
-from .cluster_ssc_ompcluster import SSCOMPPipeline
-from .cluster_ssc_admmcluster import SSCADMMPipeline
-from .cluster_ksscluster import KSSPipeline
-from .cluster_eksscluster import EKSSPipeline
-from .unsupervised_learning_grasta import GRASTAPipeline
-from .unsupervised_learning_grasta_challenge1 import GRASTAPipelineChallenge1
-from .unsupervised_learning_grasta_challenge2 import GRASTAPipelineChallenge2
-from .unsupervised_learning_grouse import GROUSEPipeline
-from .dimensionality_reduction_go_dec import GO_DECPipeline
-from .dimensionality_reduction_pcp_ialm import PCP_IALMPipeline
-from .dimensionality_reduction_rpca_lbd import RPCA_LBDPipeline
-
-
-__all__ = ["FeaturizationAudioFeaturizationPipeline",
-           "FeaturizationI3DPipeline",
-           "FeaturizationVGG16Pipeline",
-           "SSCCVXPipeline",
-           "SSCOMPPipeline",
-           "SSCADMMPipeline",
-           "OWLRegressionPipeline",
-           "OWLRegressionPipelineChallenge1",
-           "OWLRegressionPipelineChallenge2",
-           "OWLRegressionPipelineChallenge3",
-           "KSSPipeline",
-           "EKSSPipeline",
-           "GRASTAPipeline",
-           "GRASTAPipelineChallenge1",
-           "GRASTAPipelineChallenge2",
-           "GROUSEPipeline",
-           "GO_DECPipeline",
-           "PCP_IALMPipeline",
-           "RPCA_LBDPipeline"]
+from .FeaturizationAudioFeaturizationPipeline import FeaturizationAudioFeaturizationPipeline
+from .FeaturizationI3DPipeline import FeaturizationI3DPipeline
+from .FeaturizationVGG16Pipeline import FeaturizationVGG16Pipeline
+from .OWLRegressionPipeline import OWLRegressionPipeline
+from .OWLRegressionPipelineChallenge1 import OWLRegressionPipelineChallenge1
+from .OWLRegressionPipelineChallenge2 import OWLRegressionPipelineChallenge2
+from .OWLRegressionPipelineChallenge3 import OWLRegressionPipelineChallenge3
+from .SSCCVXPipeline import SSCCVXPipeline
+from .SSCOMPPipeline import SSCOMPPipeline
+from .SSCADMMPipeline import SSCADMMPipeline
+from .KSSPipeline import KSSPipeline
+from .EKSSPipeline import EKSSPipeline
+from .GRASTAPipeline import GRASTAPipeline
+from .GRASTAPipelineChallenge1 import GRASTAPipelineChallenge1
+from .GRASTAPipelineChallenge2 import GRASTAPipelineChallenge2
+from .GROUSEPipeline import GROUSEPipeline
+from .GO_DECPipeline import GO_DECPipeline
+from .PCP_IALMPipeline import PCP_IALMPipeline
+from .RPCA_LBDPipeline import RPCA_LBDPipeline
