2487c8333adddfb437f2c7b3d05ce751ed434b3e
Ryan Szeto <szetor@umich.edu>
2020-01-16 19:24:15 -0500

    Removed GO_DEC pipeline since it takes too long

--- michigan-private/genjson.py
+++ michigan-private/genjson.py
@@ -38,7 +38,7 @@ pipeline_cmd_template_info = {
     OWLRegressionPipelineChallenge2: 'd3m.primitives.regression.owl_regression.Umich',
     OWLRegressionPipelineChallenge3: 'd3m.primitives.regression.owl_regression.Umich',
     FeaturizationVGG16Pipeline: 'd3m.primitives.feature_extraction.vgg16.Umich',
-    GO_DECPipeline: 'd3m.primitives.data_compression.go_dec.Umich',
+    # GO_DECPipeline: 'd3m.primitives.data_compression.go_dec.Umich',
     PCP_IALMPipeline: 'd3m.primitives.data_compression.pcp_ialm.Umich',
     RPCA_LBDPipeline: 'd3m.primitives.data_compression.rpca_lbd.Umich',
     # GROUSEPipeline: 'd3m.primitives.data_compression.grouse.Umich',
