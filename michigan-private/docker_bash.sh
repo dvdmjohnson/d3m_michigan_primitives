55700daa34c5234d1e87f7569886114b9c226079
Ryan Szeto <szetor@umich.edu>
2020-01-19 19:08:53 -0500

    Replace Docker container suffix _test with _szetor

--- michigan-private/docker_bash.sh
+++ michigan-private/docker_bash.sh
@@ -3,9 +3,11 @@
 # Launches a bash terminal inside the Docker image. Make sure that the Docker image is running (with docker_restart.sh)
 # before calling this script.
 
-if [ -z "$(docker ps --filter 'name=spider_test' -q)" ]; then
-  echo "Docker image 'spider_test' is not running. Try running './docker_reset.sh' first."
+DOCKER_CONTAINER_NAME="spider_$USER"
+
+if [ -z "$(docker ps --filter name=$DOCKER_CONTAINER_NAME -q)" ]; then
+  echo "Docker image $DOCKER_CONTAINER_NAME is not running. Try running './docker_reset.sh' first."
   exit 1
 fi
 
-docker exec -it spider_test bash
+docker exec -it "$DOCKER_CONTAINER_NAME" bash
--- michigan-private/docker_reset.sh
+++ michigan-private/docker_reset.sh
@@ -6,19 +6,21 @@
 SCRIPT_DIR="$(cd $(dirname $0); pwd)"
 PROJ_DIR="$(cd $SCRIPT_DIR/..; pwd)"
 
+DOCKER_CONTAINER_NAME="spider_$USER"
+
 cd "$PROJ_DIR"
 # Stop Docker image if it's running
-if [ -n "$(docker ps --filter 'name=spider_test' -q)" ]; then
-  docker container stop spider_test
+if [ -n "$(docker ps --filter name=$DOCKER_CONTAINER_NAME -q)" ]; then
+  docker container stop "$DOCKER_CONTAINER_NAME"
 fi
 # Delete Docker image if it exists
-if [ -n "$(docker ps -a | grep spider_test)" ]; then
-  docker container rm spider_test
+if [ -n "$(docker ps -a | grep $DOCKER_CONTAINER_NAME)" ]; then
+  docker container rm "$DOCKER_CONTAINER_NAME"
 fi
 
 docker run -d \
   -it \
-  --name spider_test \
+  --name "$DOCKER_CONTAINER_NAME" \
   --mount type=bind,source="$(pwd)",target=/spider \
   --mount type=bind,source=/z/mid/D3M/datasets_public,target=/datasets \
   --mount type=bind,source=/z/mid/D3M/volumes,target=/volumes \
