89252418243ac900136902412546a43633050692
Ryan Szeto <szetor@umich.edu>
2020-01-23 10:37:57 -0500

    Don't export primitives with missing pipelines

--- setup.py
+++ setup.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
 
-import setuptools.command.build_py
-import distutils.cmd
-import distutils.log
 import setuptools
-import subprocess
-import unittest
-import os, os.path
+import os
 import sys
 import urllib.request, urllib.parse, urllib.error
-
-
-
 from setuptools.command.develop import develop
-from setuptools.command.install import install
 
 def spider_dl_progress_percentage(count, blocksize, totalsize):
     """ Outputs percentage progress to stdout to track download progress
@@ -142,34 +133,36 @@ setuptools.setup(
         "torchvision (==0.4.2)",
         "cvxpy (>=1.0.23)",
     ],
-    packages=["spider",
-                "spider.preprocessing",
-                "spider.preprocessing.trecs",
-                "spider.featurization",
-                "spider.featurization.vgg16", 
-                "spider.featurization.audio_featurization",
-                "spider.featurization.logmelspectrogram",
-                "spider.featurization.audio_slicer",
-                "spider.featurization.i3d",
-                "spider.distance",
-                "spider.distance.rfd",
-                "spider.cluster",
-                "spider.cluster.ssc_cvx",
-                "spider.cluster.ssc_admm",
-                "spider.cluster.kss",
-                "spider.cluster.ekss",
-                "spider.cluster.ssc_omp",
-                "spider.dimensionality_reduction",
-                "spider.dimensionality_reduction.pcp_ialm",
-                "spider.dimensionality_reduction.go_dec",
-                "spider.dimensionality_reduction.rpca_lbd",
-                "spider.supervised_learning",
-                "spider.supervised_learning.owl",
-                "spider.supervised_learning.goturn",
-                "spider.unsupervised_learning.grasta",
-                "spider.unsupervised_learning.grasta_masked",
-                "spider.unsupervised_learning.grouse",
-                "spider.pipelines"],
+    packages=[
+        "spider",
+        "spider.preprocessing",
+        "spider.preprocessing.trecs",
+        "spider.featurization",
+        "spider.featurization.vgg16",
+        "spider.featurization.audio_featurization",
+        "spider.featurization.logmelspectrogram",
+        "spider.featurization.audio_slicer",
+        "spider.featurization.i3d",
+        "spider.distance",
+        "spider.distance.rfd",
+        "spider.cluster",
+        "spider.cluster.ssc_cvx",
+        "spider.cluster.ssc_admm",
+        "spider.cluster.kss",
+        "spider.cluster.ekss",
+        "spider.cluster.ssc_omp",
+        "spider.dimensionality_reduction",
+        "spider.dimensionality_reduction.pcp_ialm",
+        "spider.dimensionality_reduction.go_dec",
+        "spider.dimensionality_reduction.rpca_lbd",
+        "spider.supervised_learning",
+        "spider.supervised_learning.owl",
+        "spider.supervised_learning.goturn",
+        "spider.unsupervised_learning.grasta",
+        "spider.unsupervised_learning.grasta_masked",
+        "spider.unsupervised_learning.grouse",
+        "spider.pipelines"
+    ],
     keywords='d3m_primitive',
     #scripts    =[],
     #ext_modules=[],
@@ -178,30 +171,29 @@ setuptools.setup(
 #               'build_py': buildPyCommand,
               },
     entry_points = {
-    'd3m.primitives': [
-        'data_compression.grasta.Umich = spider.unsupervised_learning.grasta.grasta:GRASTA',
-        'data_compression.grasta_masked.Umich = spider.unsupervised_learning.grasta_masked.grasta_masked:GRASTA_MASKED',
-        'data_compression.grouse.Umich = spider.unsupervised_learning.grouse.grouse:GROUSE',
-        'data_compression.go_dec.Umich = spider.dimensionality_reduction.go_dec.go_dec:GO_DEC',
-        'data_compression.pcp_ialm.Umich = spider.dimensionality_reduction.pcp_ialm.pcp_ialm:PCP_IALM',
-        'data_compression.rpca_lbd.Umich = spider.dimensionality_reduction.rpca_lbd.rpca_lbd:RPCA_LBD',
-        'data_preprocessing.trecs.Umich = spider.preprocessing.trecs.trecs:TRECS',
-        'feature_extraction.vgg16.Umich = spider.featurization.vgg16.vgg16:VGG16',
-        'feature_extraction.audio_featurization.Umich = spider.featurization.audio_featurization.audio_featurization:AudioFeaturization',
-        'data_preprocessing.audio_slicer.Umich = spider.featurization.audio_slicer.audio_slicer:AudioSlicer',
-        'feature_extraction.log_mel_spectrogram.Umich = spider.featurization.logmelspectrogram.logmelspectrogram:LogMelSpectrogram',
-        'feature_extraction.i3d.Umich = spider.featurization.i3d.i3d:I3D',
-        'similarity_modeling.rfd.Umich = spider.distance.rfd.rfd:RFD',
-        'clustering.ekss.Umich = spider.cluster.ekss.ekss:EKSS',
-        'clustering.kss.Umich = spider.cluster.kss.kss:KSS',
-        'clustering.ssc_admm.Umich = spider.cluster.ssc_admm.ssc_admm:SSC_ADMM',
-        'clustering.ssc_cvx.Umich = spider.cluster.ssc_cvx.ssc_cvx:SSC_CVX',
-        'clustering.ssc_omp.Umich = spider.cluster.ssc_omp.ssc_omp:SSC_OMP',
-        'regression.owl_regression.Umich = spider.supervised_learning.owl.owl:OWLRegression',
-        'learner.goturn.Umich = spider.supervised_learning.goturn.goturn:GoTurn'
+        'd3m.primitives': [
+            'data_compression.grasta.Umich = spider.unsupervised_learning.grasta.grasta:GRASTA',
+            # 'data_compression.grasta_masked.Umich = spider.unsupervised_learning.grasta_masked.grasta_masked:GRASTA_MASKED',
+            # 'data_compression.grouse.Umich = spider.unsupervised_learning.grouse.grouse:GROUSE',
+            # 'data_compression.go_dec.Umich = spider.dimensionality_reduction.go_dec.go_dec:GO_DEC',
+            # 'data_compression.pcp_ialm.Umich = spider.dimensionality_reduction.pcp_ialm.pcp_ialm:PCP_IALM',
+            # 'data_compression.rpca_lbd.Umich = spider.dimensionality_reduction.rpca_lbd.rpca_lbd:RPCA_LBD',
+            # 'data_preprocessing.trecs.Umich = spider.preprocessing.trecs.trecs:TRECS',
+            # 'feature_extraction.vgg16.Umich = spider.featurization.vgg16.vgg16:VGG16',
+            # 'feature_extraction.audio_featurization.Umich = spider.featurization.audio_featurization.audio_featurization:AudioFeaturization',
+            # 'data_preprocessing.audio_slicer.Umich = spider.featurization.audio_slicer.audio_slicer:AudioSlicer',
+            # 'feature_extraction.log_mel_spectrogram.Umich = spider.featurization.logmelspectrogram.logmelspectrogram:LogMelSpectrogram',
+            # 'feature_extraction.i3d.Umich = spider.featurization.i3d.i3d:I3D',
+            # 'similarity_modeling.rfd.Umich = spider.distance.rfd.rfd:RFD',
+            # 'clustering.ekss.Umich = spider.cluster.ekss.ekss:EKSS',
+            # 'clustering.kss.Umich = spider.cluster.kss.kss:KSS',
+            # 'clustering.ssc_admm.Umich = spider.cluster.ssc_admm.ssc_admm:SSC_ADMM',
+            # 'clustering.ssc_cvx.Umich = spider.cluster.ssc_cvx.ssc_cvx:SSC_CVX',
+            # 'clustering.ssc_omp.Umich = spider.cluster.ssc_omp.ssc_omp:SSC_OMP',
+            'regression.owl_regression.Umich = spider.supervised_learning.owl.owl:OWLRegression',
+            # 'learner.goturn.Umich = spider.supervised_learning.goturn.goturn:GoTurn'
         ],
     },
-
     test_suite='spider.tests.suite'
 )
 
