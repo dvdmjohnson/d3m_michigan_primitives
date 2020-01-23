594799bdba7bb9e59c884131ca56cd64975444ac
Ryan Szeto <szetor@umich.edu>
2020-01-23 10:11:12 -0500

    Update common-primitives and sklearn wrapper deps

--- michigan-private/install_dependencies.sh
+++ michigan-private/install_dependencies.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR="$(cd $(dirname $0); pwd)"
 PROJ_DIR="$(cd $SCRIPT_DIR/..; pwd)"
 
 cd "$PROJ_DIR"
-pip3 install --upgrade-strategy only-if-needed -e git+https://gitlab.com/datadrivendiscovery/common-primitives.git@32508af64512aa0151c8358a0a18c0af5ae18418#egg=common_primitives
+pip3 install --upgrade --upgrade-strategy only-if-needed -e git+https://gitlab.com/datadrivendiscovery/common-primitives.git@b06bc80d930e97b1c69eab860cbad35b2de17dfe#egg=common_primitives
+pip3 install --upgrade --upgrade-strategy only-if-needed -e git+https://gitlab.com/datadrivendiscovery/sklearn-wrap.git@4a2cfd1dc749bb13ce807b2bf2436a45cd49c695#egg=sklearn-wrap
 pip3 install --upgrade --upgrade-strategy only-if-needed --no-cache-dir -e .
-pip3 install -e git+https://gitlab.com/datadrivendiscovery/sklearn-wrap.git@889f93af9439fbeb29db961b03e46eaa9e2a7888#egg=sklearn-wrap
